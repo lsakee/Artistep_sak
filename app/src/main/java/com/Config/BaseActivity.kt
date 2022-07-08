d7463e18519890e95809f7d3e2d5940f4ff7e280
lsakee <lsls4868@gmail.com>
2022-07-08 19:44:54 +0900

    Initial commit

---
+++ app/src/main/java/com/Config/BaseActivity.kt
@@ -0,0 +1,20 @@
+package Guide.suchelin.config
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewbinding.ViewBinding
+
+// 액티비티의 기본을 작성, 뷰 바인딩 활용
+abstract class BaseActivity<B : ViewBinding>(private val inflate: (LayoutInflater) -> B) :
+    AppCompatActivity() {
+    protected lateinit var binding: B
+        private set
+    // 뷰 바인딩 객체를 받아서 inflate해서 화면을 만들어줌.
+    // 즉 매번 onCreate에서 setContentView를 하지 않아도 됨
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
---
+++ app/src/main/java/com/artistep/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.artistep
+
+import Guide.suchelin.config.BaseActivity
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.artistep.databinding.ActivityMainBinding
+
+class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
